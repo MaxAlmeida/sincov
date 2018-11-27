0476e8647d9954808e33579affab8d0f88716c34
MaxAlmeida <llewxam150@gmail.com>
2018-11-27 18:05:52 -0200

    Adding some configurations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_27_193919) do
+ActiveRecord::Schema.define(version: 2018_11_27_195348) do
 
   create_table "budget_products", force: :cascade do |t|
     t.string "quantity"
@@ -44,6 +44,8 @@ ActiveRecord::Schema.define(version: 2018_11_27_193919) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal "telephone"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
