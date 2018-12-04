23dceea7bee35fe63fb9eed8207c23dfbdc4d4c8
MaxAlmeida <llewxam150@gmail.com>
2018-12-03 22:38:23 -0200

    Fixing session controller

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,19 +4,21 @@ class Users::SessionsController < Devise::SessionsController
   respond_to :json
 
   def create
-    resource = User.find_for_database_authentication(email: params[:user][:email])
-    if(resource)
-      if resource.valid_password?(params[:user][:password])
-        sign_in(resource_name, resource)
-        redirect_to root_path
-      end  
-      else
-        invalid_login_attempt
-    end
- end
+  resource = User.find_for_database_authentication(email: params[:user][:email])
+  return invalid_login_attempt unless resource
+
+  if resource.valid_password?(params[:user][:password]) and resource
+    sign_in(resource_name, resource)
+    redirect_to root_path
+  else
+    invalid_login_attempt
+  end
+  end
+
 
  def invalid_login_attempt
    set_flash_message(:alert, :invalid)
+
   render json: flash[:alert], status: 401
 end
 
